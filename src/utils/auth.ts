7ab59604d5e16fa221f306aa01694adcf090a9af
sudogem <sudogem@gmail.com>
2026-01-29 00:29:40 +0800

    Fix src/utils/auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,9 +3,8 @@ import GitHub from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma"
 import Google from "next-auth/providers/google"
-import type { PrismaClient } from "@prisma/client"
-
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma as unknown as PrismaClient),
+  adapter: PrismaAdapter(prisma as unknown as never),
   providers: [GitHub, Google],
 })
